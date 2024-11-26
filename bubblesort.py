11f50d653b42d6eea9811caac4c1ebe22d5b7ec1
Nick <nicholashanson27@outlook.com>
2024-11-27 06:38:58 +0800

    modified bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -2,13 +2,16 @@ def bubblesort_(data):
         
     # checkers for sorting
     sorted = False
+    # set to 1 on each pass with swap
     sorting_flag = 0
     
     while sorted == False:
         for i in range( len ( data ) - 1 ):
+            # False: no swap
             yield data, i, False
             # perform a swap
             if data[ i ] > data[ i + 1 ]:
+                # True: swap
                 yield data, i, True
                 data[ i ], data[ i + 1 ] = data[ i + 1 ], data[ i ]
                 # still not sorted
